0443a4b4ccdbabab6fe9977f7f847d9dc31a8d30
Francisco Rivas <frankie7413@gmail.com>
2015-02-08 00:01:05 -0800

    First Commit

---
+++ SQLConnection.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace Airline_Semester_Project_attempt4
+{
+
+    //http://dev.mysql.com/downloads/connector/net/ if you get problems with not having Mysql.data installed
+
+    /// <summary>
+    /// Singleton class for SQLConnection so we only call it once per form and class to open, close and get
+    /// connections in our sql commands to database
+    /// 
+    /// When applied to a class, the sealed modifier prevents other classes from inheriting from it. In the following example, class B inherits from class A, but no class can inherit from class B.
+    /// class A {}    
+    //sealed class B : A { }
+    /// </summary>
+    public sealed class SQLConnection    //singleton class takes care of our connection needs throughout each form
+    {
+
+        private const string myConnectionString = "server=192.185.195.166;uid=comic_francisco2;" +
+            "pwd=51501fr7;database=comic_kanye1;";
+
+        private MySqlConnection connection;
+
+        private static readonly SQLConnection instance = new SQLConnection();
+
+        public static SQLConnection Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        static SQLConnection()
+        {
+
+        }
+
+        // do constructor logic here
+        public SQLConnection()
+        {
+            try
+            {
+                connection = new MySqlConnection(myConnectionString);
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally // if connection fails, close it.
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+
+            //connection.ConnectionString = myConnectionString;
+        }
+
+
+        // Open connection
+        public void OpenConnection()
+        {
+            connection.Open();
+        }
+
+        // Close connection
+        public void CloseConnection()
+        {
+            connection.Close();
+        }
+
+        // Get the connection
+
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
---
+++ SQLConnectionNotSingleton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Airline_Semester_Project_attempt4
+{
+    class SQLConnection
+    {
+
+        private const string myConnectionString = "server=192.185.195.166;uid=comic_francisco2;" +
+            "pwd=51501fr7;database=comic_kanye2;";
+
+        private MySqlConnection connection;
+
+        // constructor
+        public SQLConnection()
+        {
+            connection = new MySqlConnection();
+            connection.ConnectionString = myConnectionString;
+        }
+
+        //public MySqlConnection GetConnection()
+        //{
+        //    connection = new MySqlConnection();
+        //    connection.ConnectionString = myConnectionString;
+        //    return connection;
+        //}
+        
+        // Open connection
+        public void OpenConnection()
+        {
+            connection.Open();
+        }
+
+        // Close connection
+        public void CloseConnection()
+        {
+            connection.Close();
+        }
+            
+        public MySqlConnection Connection { get; set; }
+
+    }
+}
